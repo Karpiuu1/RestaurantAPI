2ebc08842181cf19a531e2483ebd69fa7cf8d574
Adrian <karpinski.adrian1@gmail.com>
2023-06-22 20:09:15 +0200

    Enitity

---
+++ Entities/RestaurantDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace RestaurantAPI.Entities
+{
+    public class RestaurantDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurant { get; set; }
+        public DbSet<Address> Address { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Restaurant>()
+                .Property(r => r.Name)
+        }
+    }
+}
