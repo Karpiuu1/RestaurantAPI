9f5a7553077b407d19d25168aa3155aa90046e17
Adrian <karpinski.adrian1@gmail.com>
2023-06-24 16:48:10 +0200

    Database fix

--- Entities/RestaurantDbContext.cs
+++ Entities/RestaurantDbContext.cs
@@ -10,14 +10,29 @@ namespace RestaurantAPI.Entities
 {
     public class RestaurantDbContext : DbContext
     {
-        public DbSet<Restaurant> Restaurant { get; set; }
-        public DbSet<Address> Address { get; set; }
+        private string _connectionString =
+            "Server=(localdb)\\mssqllocaldb;Database=RestaurantDb;Trusted_Connection=True;";
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Address> Addresses { get; set; }
         public DbSet<Dish> Dishes { get; set; }
+   
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            
             modelBuilder.Entity<Restaurant>()
                 .Property(r => r.Name)
+                .IsRequired()
+                .HasMaxLength(25);
+
+            modelBuilder.Entity<Dish>()
+                .Property(d => d.Name)
+                .IsRequired();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
         }
     }
 }
