79e212063fc4a68cac818d6d7af653674de08a8c
Adrian <karpinski.adrian1@gmail.com>
2023-06-27 19:53:16 +0200

    HttpPut controller add

--- Services/RestaurantService.cs
+++ Services/RestaurantService.cs
@@ -14,6 +14,7 @@ namespace RestaurantAPI.Services
         IEnumerable<RestaurantDto> GetAll();
         int Create(CreateRestaurantDto dto);
         bool Delete(int id);
+        bool Update(int id, UpdateRestaurantDto dto);
     }
     
     public class RestaurantService :  IRestaurantService
@@ -27,13 +28,28 @@ namespace RestaurantAPI.Services
             _mapper = mapper;
         }
 
+        public bool Update(int id, UpdateRestaurantDto dto)
+        {
+            var restaurant = _dbContext
+                .Restaurants
+                .FirstOrDefault(r => r.Id == id);
+
+            if (restaurant is null) return false;
+
+            restaurant.Name = dto.Name;
+            restaurant.Description = dto.Description;
+            restaurant.HasDelivery = dto.HasDelivery;
+
+            _dbContext.SaveChanges();
+            return true;
+        }
         public bool Delete(int id)
         {
             var restaurant = _dbContext
                 .Restaurants
                 .FirstOrDefault(r => r.Id == id);
 
-            if (restaurant == null) return false;
+            if (restaurant is null) return false;
 
             _dbContext.Restaurants .Remove(restaurant);
             _dbContext.SaveChanges();
