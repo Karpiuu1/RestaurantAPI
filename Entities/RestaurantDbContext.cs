78bfc3186687b46a1372b2b07c8ca7722aa2c0a3
Adrian <karpinski.adrian1@gmail.com>
2023-06-27 19:27:02 +0200

    Controller Refactor

--- Entities/RestaurantDbContext.cs
+++ Entities/RestaurantDbContext.cs
@@ -28,6 +28,7 @@ namespace RestaurantAPI.Entities
             modelBuilder.Entity<Dish>()
                 .Property(d => d.Name)
                 .IsRequired();
+
             modelBuilder.Entity<Address>()
                 .Property(a => a.City)
                 .IsRequired()
