4532aa08138e20b8dfed2e127997595846efc499
Adrian <karpinski.adrian1@gmail.com>
2023-07-10 19:12:25 +0200

    Results Sorting

--- Models/Validators/RestaurantQueryValidator.cs
+++ Models/Validators/RestaurantQueryValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using RestaurantAPI.Entities;
 using System;
 using System.Linq;
 
@@ -7,6 +8,7 @@ namespace RestaurantAPI.Models.Validators
     public class RestaurantQueryValidator : AbstractValidator<RestaurantQuery>
     {
         private int[] allowedPageSizes = new[] { 5, 10, 15 };
+        private string[] allowedSortByColumnNames = { nameof(Restaurant.Name), nameof(Restaurant.Category), nameof(Restaurant.Description) };
         public RestaurantQueryValidator()
         {
             RuleFor(r => r.PageNumber).GreaterThan(1);
@@ -17,6 +19,11 @@ namespace RestaurantAPI.Models.Validators
                     context.AddFailure("PageSize",$"PageSize must be in [{string.Join(",", allowedPageSizes)}]");
                 }
             });
+
+            RuleFor(r => r.SortBy)
+                .Must(value => string.IsNullOrEmpty(value) || allowedSortByColumnNames.Contains(value))
+                .WithMessage($"Sort by is optional, or must be in [{string.Join(",", allowedSortByColumnNames)}]");
+                
         }
     }
 }
--- Services/RestaurantService.cs
+++ Services/RestaurantService.cs
@@ -7,9 +7,11 @@ using RestaurantAPI.Authorization;
 using RestaurantAPI.Entities;
 using RestaurantAPI.Exceptions;
 using RestaurantAPI.Models;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Security.Claims;
 
 namespace RestaurantAPI.Services
@@ -119,8 +121,23 @@ namespace RestaurantAPI.Services
                  .Include(r => r.Address)
                  .Include(r => r.Dishes)
                  .Where(r => query.SearchPhrase == null || (r.Name.ToLower().Contains(query.SearchPhrase.ToLower())
-                                                 || r.Description.ToLower().Contains(query.SearchPhrase.ToLower())));
+                                                        || r.Description.ToLower().Contains(query.SearchPhrase.ToLower())));
 
+            if (string.IsNullOrEmpty(query.SortBy))
+            {
+                var columnsSelectors = new Dictionary<string, Expression<Func<Restaurant, object>>>
+                {
+                    { nameof(Restaurant.Name), r => r.Name },
+                    { nameof(Restaurant.Description), r => r.Name },
+                    { nameof(Restaurant.Category), r => r.Name },
+                };
+
+                var selectedColumn = columnsSelectors[query.SortBy];
+
+                baseQuery =query.SortDirection == SortDirection.ASC 
+                    ? baseQuery.OrderBy(selectedColumn)
+                    : baseQuery.OrderByDescending(selectedColumn);
+            }
 
             var restaurants = baseQuery
                 .Skip(query.PageSize * (query.PageNumber - 1))
