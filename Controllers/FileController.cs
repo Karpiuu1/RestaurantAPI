df3758cf8a66a1a51bad55261731db1d4f6604a8
Adrian <karpinski.adrian1@gmail.com>
2023-07-11 18:03:30 +0200

    FIleUpload

--- Controllers/FileController.cs
+++ Controllers/FileController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.StaticFiles;
 
@@ -32,6 +33,25 @@ namespace RestaurantAPI.Controllers
             var fileContents = System.IO.File.ReadAllBytes(filePath);
 
             return File(fileContents, contentType, fileName);
+        }
+        [HttpPost]
+        public ActionResult Upload([FromForm]IFormFile file)
+        {
+            if(file != null && file.Length > 0)
+            {
+                var rootPath = Directory.GetCurrentDirectory();
+                var fileName = file.FileName;
+                var fullPath = $"{rootPath}/PrivateFiles/{fileName}";
+
+                using(var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                return Ok();
+            }
+
+            return BadRequest();
         }
     }
 }
